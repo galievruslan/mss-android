6ceb6827341df50c9c87e7abe1b3727bc7b70020
golfarid <golfarid@gmail.com>
2013-06-28 19:09:29 +0400

    WebRepositories development

--- mss.android.tests/src/com/mss/android/web/repositories/WebServerTest.java
+++ mss.android.tests/src/com/mss/android/web/repositories/WebServerTest.java
@@ -1,18 +1,15 @@
 package com.mss.android.web.repositories;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class WebServerTest {
 
 	@Test
-	public void test() {
+	public void testWebServerAuthentication() {
 		WebServer webServer = new WebServer("http://mss.alkotorg.com:3000");
 		try {
-			WebConnection connection = webServer.connect("manager", "423200");
-		} catch (WebConnectionException e) {
-			// TODO Auto-generated catch block
+			webServer.connect("admin", "423200");
+		} catch (WebConnectionException | AuthenticationFailedException e) {
 			e.printStackTrace();
 		}
 	}
--- mss.android.tests/src/mss/android/infrastructure/ormlite/OrmLiteTest.java
+++ mss.android.tests/src/mss/android/infrastructure/ormlite/OrmLiteTest.java
@@ -1,7 +1,5 @@
 package mss.android.infrastructure.ormlite;
 
-import static org.junit.Assert.*;
-
 import java.util.Iterator;
 
 import org.junit.After;
