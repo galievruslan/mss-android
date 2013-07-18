450016d826cb06303e9ef15a95be492d22af3733
golfarid <golfarid@gmail.com>
2013-07-18 15:27:22 +0400

    Add AuthenticationService which wraps AccountManager calls

---
+++ mss/src/com/mss/application/services/Credentials.java
@@ -0,0 +1,20 @@
+package com.mss.application.services;
+
+public class Credentials {
+	
+	private String login; 
+	private String password;
+	
+	public Credentials(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+	
+	public String getLogin(){
+		return login;
+	}	
+	
+	public String getPassword() {
+		return password;
+	}
+}
